211f9b5282f9f82e8e9be51ff855d7f7daf7adb7
wanr000 <simg629@gmail.com>
2024-11-11 23:11:01 +0300

    init

--- lab_5/lab_5/main.c
+++ lab_5/lab_5/main.c
@@ -1,14 +1,5 @@
-//
-//  main.c
-//  lab_5
-//
+
 //  Created by Роман on 11.11.24.
 //
 
-#include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
-}
