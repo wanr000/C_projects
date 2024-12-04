eb711b81bf4016e4d4579d8b5a1d395093f3abf6
wanr000 <simg629@gmail.com>
2024-12-04 12:48:17 +0300

    init

--- lab6/lab6/functions.h
+++ lab6/lab6/functions.h
@@ -26,7 +26,6 @@ int start_1(char b){
     printf("Матрица заполняется случайными числами(a), вводимыми(b) или из файла(c): ");
     while (1) {
         char input[100];
-//        scanf("%s", &input);
         // Указываем массив в который считывается строка, максимальное количество символов, поток из которого читаются данные
         fgets(input, sizeof(input), stdin);
         // Вместо символа новой строки, ставим символ конца строки
@@ -108,9 +107,9 @@ void print_arr2(int n, int *m, double ** a){
     }
 }
 
-void print_arr3(int n, int *m, int ** a){
+void print_arr3(int n, int m, int ** a){
     for(int i =0; i < n; i++){
-        for(int j = 0; j < m[i]; j++){
+        for(int j = 0; j < m; j++){
             printf("%d  ", a[i][j]);
         }
         printf("\n");
@@ -155,3 +154,59 @@ void bubble_sort(int * a, int size){
     free(new_a);
 }
 
+
+int find_max_in_column(int **matrix, int rows, int col) {
+    int max = matrix[0][col];
+    for (int i = 1; i < rows; i++) {
+        if (matrix[i][col] > max) {
+            max = matrix[i][col];
+        }
+    }
+    return max;
+}
+
+void shell_sort_columns(int **matrix, int rows, int cols) {
+    int *max_values = (int*)malloc(cols * sizeof(int));
+    for (int j = 0; j < cols; j++) {
+        max_values[j] = find_max_in_column(matrix, rows, j);
+    }
+
+    for (int gap = cols / 2; gap > 0; gap /= 2) {
+        for (int i = gap; i < cols; i++) {
+            int temp_max = max_values[i];
+            int *temp_col = (int*)malloc(rows * sizeof(int));
+            for (int k = 0; k < rows; k++) {
+                temp_col[k] = matrix[k][i];
+            }
+            int j;
+            for (j = i; j >= gap && max_values[j - gap] < temp_max; j -= gap) {
+                max_values[j] = max_values[j - gap];
+                for (int k = 0; k < rows; k++) {
+                    matrix[k][j] = matrix[k][j - gap];
+                }
+            }
+            max_values[j] = temp_max;
+            for (int k = 0; k < rows; k++) {
+                matrix[k][j] = temp_col[k];
+            }
+            free(temp_col);
+        }
+    }
+    free(max_values);
+}
+
+int choice_task(int task){
+    char input[100];
+    while (1) {
+        fgets(input, sizeof(input), stdin);
+        input[strcspn(input, "\n")] = '\0';
+        if (isValidInput(input) && atoi(input) > 0){
+            sscanf(input, "%d", &task);
+            break;
+        }
+        else{
+            printf("Некорректный ввод. Попробуйте еще раз: ");
+        }
+    }
+    return task;
+}
--- lab6/lab6/main.c
+++ lab6/lab6/main.c
@@ -5,74 +5,69 @@
 #include <ctype.h>
 #include "functions.h"
 
-//// Задание 1
-//int main(){
-//    char b = start_1(b);
-//    int n = start_2(n);
-//    int * a = malloc(n * sizeof(int));
-//    int a_1;
-//
-//    if(b == 'a'){
-//        FILE *file = fopen("array.txt", "w");
-//        srand(time(NULL));
-//        for(int k=0;k<n; k++){
-//            a[k]= (-50 + rand()%100);
-//            fprintf(file, "%d  ", a[k]);
-//        }
-//        fclose(file);
-//    }
-//    else if(b == 'b'){
-//        FILE *file = fopen("array.txt", "w");
-//        int nms;
-//        for(int k=0;k<n;k++){
-//            while (1) {
-//                nms = scanf("%d", &a_1);
-//                if (nms == 1) {
-//                    break;
-//                }
-//                else {
-//                    printf("Введен некорректный символ, введите еще раз: ");
-//                    while (getchar() != '\n');
-//                }
-//            }
-//            a[k] = a_1;
-//            fprintf(file, "%d  ", a[k]);
-//            }
-//            fclose(file);
-//    }
-//    else if(b == 'c'){
-//        FILE *file = fopen("array.txt", "r");
-//        for(int k=0;k<n; k++){
-//            fscanf(file, "%d", &a[k]);
-//        }
-//        fclose(file);
-//    }
-//
-//    printf("Исходный массив: ");
-//    print_arr(a, n);
-//
-//    bubble_sort(a, n);
-//
-//    printf("Измененный массив: ");
-//    print_arr(a, n);
-//    free(a);
-//
-//    return 0;
-//}
+// Задание 1
+void task1(){
+    char b = start_1(b);
+    int n = start_2(n);
+    int * a = malloc(n * sizeof(int));
+    int a_1;
 
+    if(b == 'a'){
+        FILE *file = fopen("array.txt", "w");
+        srand(time(NULL));
+        for(int k=0;k<n; k++){
+            a[k]= (-50 + rand()%100);
+            fprintf(file, "%d  ", a[k]);
+        }
+        fclose(file);
+    }
+    else if(b == 'b'){
+        FILE *file = fopen("array.txt", "w");
+        int nms;
+        for(int k=0;k<n;k++){
+            while (1) {
+                nms = scanf("%d", &a_1);
+                if (nms == 1) {
+                    break;
+                }
+                else {
+                    printf("Введен некорректный символ, введите еще раз: ");
+                    while (getchar() != '\n');
+                }
+            }
+            a[k] = a_1;
+            fprintf(file, "%d  ", a[k]);
+            }
+            fclose(file);
+    }
+    else if(b == 'c'){
+        FILE *file = fopen("array.txt", "r");
+        for(int k=0;k<n; k++){
+            fscanf(file, "%d", &a[k]);
+        }
+        fclose(file);
+    }
 
+    printf("Исходный массив: ");
+    print_arr(a, n);
+
+    bubble_sort(a, n);
+
+    printf("Измененный массив: ");
+    print_arr(a, n);
+    free(a);
+
+}
 
 // Задание 2
-int main(){
+void task2(){
     char b = start_1(b);
     int n = start_22(n);
     int m = start_23(m);
     
     int ** array = (int**) calloc(n, sizeof(int*));
-    int * cols = (int*)malloc(n * sizeof(int));
     for (int i = 0; i < n; i++) {
         array[i] = (int*) calloc(m, sizeof(int));
-        cols[i] = m;
     }
     
 
@@ -83,7 +78,6 @@ int main(){
             for(int j=0;j<m; j++){
                 array[k][j] = (-20 + rand()%100);
                 fprintf(file, "%d  ", array[k][j]);
-                array[k][m-1]=-100;
             }
             fprintf(file, "\n");
         }
@@ -104,7 +98,6 @@ int main(){
                         while (getchar() != '\n');
                     }
                 }
-                array[k][m-1]=-100;
                 fprintf(file, "%d  ", array[k][j]);
             }
             fprintf(file, "\n");
@@ -116,7 +109,6 @@ int main(){
         for(int k=0;k<n; k++){
             for(int j=0;j<m; j++){
                 fscanf(file, "%d", &array[k][j]);
-                array[k][m-1]=-100;
             }
             fprintf(file, "\n");
         }
@@ -124,19 +116,37 @@ int main(){
     }
     
     printf("Исходный массив: \n");
-    print_arr3(n, cols, array);
+    print_arr3(n, m, array);
     
-    
-
+    shell_sort_columns(array, n, m);
     printf("\nИзмененный массив: \n");
-    print_arr3(n, cols, array);
+    print_arr3(n, m, array);
     
     for(int i = 0; i < n; i++) {
         free(array[i]);
     }
     free(array);
-    free(cols);
+}
+
+
+// Задание 2
+int main(){
+    printf("Выберите задание: ");
+    int task = choice_task(task);
     
-    return 0;
+    while (1) {
+        if (task == 1){
+            task1();
+            break;;
+        }
+        if (task == 2){
+            task2();
+            break;
+        }
+        else{
+            printf("Неверный номер задания. Попробуйте еще раз: ");
+            task = choice_task(task);
+        }
+    }
 }
 
