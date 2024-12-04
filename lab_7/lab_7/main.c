2ec6e8407aab2f5472c65ccea1c5cd973776aa06
wanr000 <simg629@gmail.com>
2024-12-04 14:32:58 +0300

    init

---
+++ lab_7/lab_7/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  lab_7
+//
+//  Created by Роман on 4.12.24.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
