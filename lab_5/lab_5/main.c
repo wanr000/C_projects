c9ad8d0fe28a9c5016d53afc9fc3f88cecd9ef5a
wanr000 <simg629@gmail.com>
2025-01-12 21:27:29 +0300

    init

---
+++ lab_5/lab_5/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <ctype.h>
+#include "functions.h"
+
+int main(void){
+    int n=2;
+    while (1) {
+        if(n==2){
+            start();
+            printf("\nЗакрыть программу?\n");
+            printf("1-Да\n2-Нет\n");
+            n = choice_task(n);
+        }
+
+        if (n == 1){
+            return 0;
+        }
+        if (n == 2){
+            continue;
+        }
+        else{
+            printf("Данного варианта ответа не существует. Повторите ввод: ");
+            n = choice_task(n);
+        }
+    }
+}
