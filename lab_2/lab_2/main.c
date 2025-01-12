dc2e17213c06b63cc90815e29729f84cfa6a0155
wanr000 <simg629@gmail.com>
2025-01-12 19:07:27 +0300

    init

--- lab_2/lab_2/main.c
+++ lab_2/lab_2/main.c
@@ -1,1 +1,31 @@
+#include <stdio.h>
+#include <math.h>
+#include "functions.h"
+
+int main(void){
+    int n;
+    start();
+    
+    printf("\nЗакрыть программу?\n");
+    printf("1-Да\n2-Нет\n");
+    
+    n = choice_task(n);
+    
+    while (1) {
+        if (n == 1){
+            return 0;
+        }
+        if (n == 2){
+            start();
+            printf("\nЗакрыть программу?\n");
+            printf("1-Да\n2-Нет\n");
+            n = choice_task(n);
+            
+        }
+        else{
+            printf("Неверный номер задания. Попробуйте еще раз: ");
+            n = choice_task(n);
+        }
+    }
+}
 
