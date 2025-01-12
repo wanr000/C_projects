ed1f2db1df7f57f03d5238766e09fc5f0270c3aa
wanr000 <simg629@gmail.com>
2025-01-13 02:08:21 +0300

    init

--- lab_5/lab_5/main.c
+++ lab_5/lab_5/main.c
@@ -4,6 +4,38 @@
 #include <time.h>
 #include <ctype.h>
 #include "functions.h"
+#include "task1.h"
+#include "task2.h"
+#include "task3.h"
+
+
+void start(void){
+    int task;
+    printf("Выберите задание для проверки: ");
+    
+    task = choice_task(task);
+    
+    while (1) {
+        if (task == 1){
+            task1();
+            break;;
+        }
+        if (task == 2){
+            task2();
+            break;
+        }
+        if (task == 3){
+            task3();
+            break;
+        }
+        else{
+            printf("Неверный номер задания. Попробуйте еще раз: ");
+            task = choice_task(task);
+        }
+    }
+    
+}
+
 
 int main(void){
     int n=2;
---
+++ lab_5/lab_5/task1.h
@@ -0,0 +1,66 @@
+void task1(void){
+    char b = start_1(b);
+    int n = start_2(n);
+    double * a = malloc(n * sizeof(double));
+    double a_1;
+    
+    if(b == 'a'){
+        FILE *file = fopen("array.txt", "w");
+        srand(time(NULL));
+        for(int k=0;k<n; k++){
+            int r = (0 + rand()%2);
+            if (r==1){
+                a[k]= (-20 + rand()%100);
+                fprintf(file, "%.2lf  ", a[k]);
+            }
+            else{
+                a[k]= (-20.3 + rand()%100);
+                fprintf(file, "%.2lf  ", a[k]);
+            }
+        }
+        fclose(file);
+    }
+    else if(b == 'b'){
+        FILE *file = fopen("array.txt", "w");
+        int nms;
+        for(int k=0;k<n;k++){
+            while (1) {
+                nms = scanf("%lf", &a_1);
+                if (nms == 1) {
+                    break;
+                }
+                else {
+                    printf("Введен некорректный символ, введите еще раз: ");
+                    while (getchar() != '\n');
+                }
+            }
+            a[k] = a_1;
+            fprintf(file, "%.2lf  ", a[k]);
+        }
+        fclose(file);
+    }
+    else if(b == 'c'){
+        FILE *file = fopen("array.txt", "r");
+        for(int k=0;k<n; k++){
+            fscanf(file, "%lf", &a[k]);
+        }
+        fclose(file);
+    }
+    
+    printf("Исходный массив: ");
+    print_arr(a, n);
+    for (int i = 0; i < n; i++) {
+        if (a[i] == (int)a[i]) {
+            for (int j = i; j < n - 1; j++) {
+                a[j] = a[j + 1];
+            }
+            n--;
+            i--;
+        }
+    }
+    
+    printf("Измененный массив: ");
+    print_arr(a, n);
+    free(a);
+    
+}
