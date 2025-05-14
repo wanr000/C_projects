438b2e7cf66c742d95f8bea0a158dbe3982242e1
wanr000 <simg629@gmail.com>
2025-05-14 22:12:12 +0300

    init

---
+++ prakt/prakt/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  prakt
+//
+//  Created by Роман on 14.05.25.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
